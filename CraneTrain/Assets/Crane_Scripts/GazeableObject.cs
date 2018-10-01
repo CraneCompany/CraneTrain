8e7fd9faedbe0fdd66cf81c78fa555ac28d72e7e
VRBeest <crane.kahkah@gmail.com>
2018-10-01 14:57:59 +0200

    Added Gazeable and QuestObject script. Finished Gazeable. -Lars

---
+++ CraneTrain/Assets/Crane_Scripts/GazeableObject.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class GazeableObject : MonoBehaviour
+{
+    public FoveInterfaceBase foveInterface;
+
+    private Collider my_collider;
+    private bool b_onTarget, b_eyesClosed;
+    void Start()
+    {
+        my_collider = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        //Checks whether or not the user gazes at this object
+        if (foveInterface.Gazecast(my_collider))
+        {
+            //Causes it to be a execute the code 1 time instead of every frame
+            if (!b_onTarget)
+            {
+                b_onTarget = true;
+            }
+        }
+        else
+        {
+            //Causes it to be a execute the code 1 time instead of every frame
+            if (b_onTarget)
+            {
+                b_onTarget = false;
+            }
+        }
+        //Checks if the user has its eyes closed
+        if (foveInterface.CheckEyesClosed() == Fove.Managed.EFVR_Eye.Both)
+        {
+            b_eyesClosed = true;
+        }
+        else
+        {
+            b_eyesClosed = false;    
+        }
+    }
+
+    //Public methods with just a bool to return
+    public bool OnTarget()
+    {
+        return b_onTarget;
+    }
+    public bool EyesClosed()
+    {
+        return b_eyesClosed;
+    }
+}
---
+++ CraneTrain/Assets/Crane_Scripts/QuestObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObject : MonoBehaviour {
+    private GazeableObject GazeableObject;
+
+	// Use this for initialization
+	void Start () {
+        GazeableObject = GetComponent<GazeableObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T) && GazeableObject.OnTarget())
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        } 
+	}
+}
