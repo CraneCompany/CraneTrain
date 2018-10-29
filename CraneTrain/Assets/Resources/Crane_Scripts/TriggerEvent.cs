e72e3ed39a891bed465f7cb13205c9ce6ddad74a
marc <marc.bunschoten@gmail.com>
2018-10-29 16:18:08 +0100

    start on events

---
+++ CraneTrain/Assets/Resources/Crane_Scripts/TriggerEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+    //public GameObject[] go_events;
+
+    //void OnTriggerEnter(Collider col)
+    //{
+    //    //transform.parent.GetComponent<EventHandler>();
+    //}
+
+    void OnTriggerEnter(Collider col)
+    {
+        //transform.parent.GetComponent<EventHandler>();
+    }
+
+}
