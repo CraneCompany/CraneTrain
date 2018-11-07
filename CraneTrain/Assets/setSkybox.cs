d2a88bb9905b54e3f382689d9d373e5d7f89d984
Tessa <Tessa_huijzer@hotmail.com>
2018-11-07 15:05:25 +0100

    Dynamic Skybox

---
+++ CraneTrain/Assets/setSkybox.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class setSkybox : MonoBehaviour {
+
+    Camera cam;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(SceneManager.GetActiveScene().name == "Scene1")
+        {
+            cam.clearFlags = CameraClearFlags.SolidColor;
+        }
+        else if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            cam.clearFlags = CameraClearFlags.SolidColor;
+        }
+        else
+        {
+            cam.clearFlags = CameraClearFlags.Skybox;
+        }
+	}
+}
