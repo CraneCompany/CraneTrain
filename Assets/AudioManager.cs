2620c1872322be608e21b0dba7c8e19959e93199
VRBeest <crane.kahkah@gmail.com>
2018-11-15 15:08:56 +0100

    4 event locaties toegevoegd met 2 events per stuk. 1 prefab heeft een place holder en die fiets- en politie events hebben geluid

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    private AudioSource as_audioSource;
+    public AudioClip[] ac_coinSounds = new AudioClip[4];
+    public float f_volume;
+
+	// Use this for initialization
+	void Start () {
+        as_audioSource = GameObject.FindGameObjectWithTag("GAS").GetComponent<AudioSource>();
+        ac_coinSounds[0] = Resources.Load<AudioClip>("Crane_Audio/Coin1");
+        f_volume = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(ac_coinSounds[0]);
+            as_audioSource.clip = ac_coinSounds[0];
+            as_audioSource.PlayOneShot(ac_coinSounds[0], f_volume);
+
+        }
+    }
+}
