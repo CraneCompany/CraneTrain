4d631d64fae8a890b713e4a3991f910525fb0cf5
VRBeest <crane.kahkah@gmail.com>
2018-11-30 16:23:59 +0100

    Added staretime support, optimized feedbackring and made buttonchanges in menu more dynamic.

--- CraneTrain/Assets/Resources/Crane_Scripts/AudioManager.cs
+++ CraneTrain/Assets/Resources/Crane_Scripts/AudioManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,37 +7,37 @@ public class AudioManager : MonoBehaviour
     private AudioSource as_audioSource;
     public AudioClip[] ac_coinSounds = new AudioClip[3];
     private AudioClip ac_lvlUp;
-    public float f_volume;
+    public float f_trainingVolume;
+    public float f_globalVolume;
     private int i_sCounter;
+    private GlobalParameterScript cs_globalParameterScript;
 
-    // Use this for initialization
     void Start()
     {
+        cs_globalParameterScript = GetComponent<GlobalParameterScript>();
         i_sCounter = 0;
         LoadSounds();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AudioOptionsUpdated()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            PlayCoinSound();
-        }
+        as_audioSource.volume = f_globalVolume;
     }
 
     void LoadSounds()
     {
         if (!GameObject.FindGameObjectWithTag("GAS")) return;
+        f_trainingVolume = cs_globalParameterScript.f_trainVolume;
+        f_globalVolume = cs_globalParameterScript.f_globalVolume;
 
         as_audioSource = GameObject.FindGameObjectWithTag("GAS").GetComponent<AudioSource>();
-        f_volume = 1;
         for (int i = 0; i < 3; i++)
         {
             ac_coinSounds[i] = Resources.Load<AudioClip>("Crane_Audio/Coin" + (i + 1).ToString());
         }
         ac_lvlUp = Resources.Load<AudioClip>("Crane_Audio/LvlUp");
 
+        AudioOptionsUpdated();
     }
 
     public void PlayCoinSound()
@@ -46,7 +46,7 @@ public class AudioManager : MonoBehaviour
         {
             i_sCounter = 0;
         }
-        as_audioSource.PlayOneShot(ac_coinSounds[i_sCounter], f_volume);
+        as_audioSource.PlayOneShot(ac_coinSounds[i_sCounter], f_trainingVolume);
         i_sCounter++;
     }
 
--- CraneTrain/Assets/Resources/Crane_Scripts/FeedBackRing.cs
+++ CraneTrain/Assets/Resources/Crane_Scripts/FeedBackRing.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,13 +8,13 @@ public class FeedBackRing : MonoBehaviour
     private Target cs_targetScript;
     private Transform t_childTrans;
     private Vector3 v3_myScales, v3_buScales;
-    public float f_shrinkRate = .02f;
+    public float f_shrinkTime; //in seconds
     private float f_x, f_y, f_z;
     public bool b_lookedAt;
 
-    // Use this for initialization
     void Start()
     {
+        f_shrinkTime = GameObject.Find("_GlobalGameObject").GetComponent<GlobalParameterScript>().f_stareTime;
         cs_targetScript = gameObject.GetComponent<Target>();
         t_childTrans = transform.GetChild(0);
         v3_myScales = transform.localScale;
@@ -24,29 +24,29 @@ public class FeedBackRing : MonoBehaviour
         ResetScales();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
     public void ExpandOut()
     {
-        if (Vector3.Distance(t_childTrans.localScale, v3_buScales) > .3f)
+        if (t_childTrans.localScale.x < v3_buScales.x)
         {
-            f_x += f_shrinkRate;
-            f_y += f_shrinkRate;
+            f_x += ((t_childTrans.localScale.x * v3_myScales.x) / f_shrinkTime) * Time.deltaTime;
+            f_y += ((t_childTrans.localScale.y * v3_myScales.y) / f_shrinkTime) * Time.deltaTime;
 
             t_childTrans.localScale = new Vector3(f_x, f_y, f_z);
+            if (t_childTrans.localScale.x >= v3_buScales.x)
+            {
+                ResetScales();
+            }
         }
     }
 
     public void ShrinkDown()
     {
-        f_x -= f_shrinkRate;
-        f_y -= f_shrinkRate;
+        f_x -= ((t_childTrans.localScale.x * v3_myScales.x) / f_shrinkTime) * Time.deltaTime;
+        f_y -= ((t_childTrans.localScale.y * v3_myScales.y) / f_shrinkTime) * Time.deltaTime;
 
+        Debug.Log(f_x);
         t_childTrans.localScale = new Vector3(f_x, f_y, f_z);
-        if (Vector3.Distance(t_childTrans.localScale, v3_myScales) < .5f)
+        if ((t_childTrans.localScale.x*v3_myScales.x) <= v3_myScales.x)
         {
             ResetScales();
             cs_targetScript.PlayerDestroyed();
