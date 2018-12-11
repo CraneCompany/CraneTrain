d9092800ba4446e3abb0679d757669888773a0f8
marc <marc.bunschoten@gmail.com>
2018-12-11 15:22:44 +0100

    Added walls for water, made passive event scripts

---
+++ CraneTrain/Assets/Resources/Crane_Scripts/PassiveEventSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveEventSpawn : MonoBehaviour {
+
+    public GameObject EventObject;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Trigger")
+        {
+            Instantiate(EventObject);
+        }
+    }
+}
