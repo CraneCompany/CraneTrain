486274a848ab155d4d503cb289296cbd70feb7b9
ereboszephyros <ryan.de.jong@hva.nl>
2019-01-10 15:51:01 +0100

    Politie auto met sirene en 2 auto's

---
+++ CraneTrain/Assets/Resources/Crane_Scripts/SirenEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SirenEffect : MonoBehaviour
+{
+
+    [SerializeField] Light redLight;
+    [SerializeField] Light blueLight;
+
+    private Vector3 redTemp;
+    private Vector3 blueTemp;
+
+    [SerializeField] int speed;
+
+
+    void Update()
+    {
+        redTemp.y += speed * Time.deltaTime;
+        blueTemp.y -= speed * Time.deltaTime;
+
+        redLight.transform.eulerAngles = redTemp;
+        blueLight.transform.eulerAngles = blueTemp;
+    }
+}
